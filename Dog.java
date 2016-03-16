db8051cc64623abb91e24072d3bac486ee4dd94a
Au Yuet Shing <Aulu@AudeMacBook-Air.local>
2016-03-16 11:20:21 -0400

    first commit

---
+++ Dog.java
@@ -0,0 +1,15 @@
+public class Dog{
+  
+  String breed;
+  int age;
+  String name;
+  
+  public Dog(String breed, int age, String namre){
+    this.breed = breed;
+    this.age = age;
+    this.name = name;
+  }
+}
+  
+  
+    
